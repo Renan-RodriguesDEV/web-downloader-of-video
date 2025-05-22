e6f3d8172ab94edd7c1b81b90648edbb809c620d
Renan RodriguesDev <renanrodrigues7110@gmail.com>
2025-05-22 10:36:03 -0300

    update to fixed

--- app.py
+++ app.py
@@ -9,34 +9,40 @@ st.title("Bem vindo ao Downloader, forneca a url do video!!!")
 url_video = st.text_input(
     "insira a URL referente ao video", placeholder="https://www.youtube.com/watch"
 )
+
 if st.button("Buscar"):
     result = baixar_com_ytdlp(url_video)
     if result:
         st.success("Video baixado com sucesso!")
         arquivos = os.listdir(PATH_DOWNLOADS)
         for arq in arquivos:
-            with open(f"{PATH_DOWNLOADS}/{arq}", "rb") as v:
-                if arq.endswith(".mp4"):
+            if arq.endswith(".mp4"):
+                with open(f"{PATH_DOWNLOADS}/{arq}", "rb") as v:
                     st.video(v.read())
+                    # Inicializa download como False
+                    download = False
+                    # Botão de download
                     download = st.download_button(
                         "Baixar video", data=v.read(), file_name="download.mp4"
                     )
-                cont = 0
-                placeholder = st.empty()
-                while cont <= 300:
-                    if download:
-                        break
-                    placeholder.warning(
-                        f"Tempo de download disponível: {300-cont} segundos"
-                    )
-                    cont += 1
-                    time.sleep(1)
-            # limpando a pasta de downloads do servidor
-            print(f"[INFO] - {time.strftime('%X')} - Removendo arquivo {arq} [INFO]")
-            os.remove(f"{PATH_DOWNLOADS}/{arq}")
-            st.subheader("Tempo esgotado, forneca a url novamente")
-            time.sleep(5)
-            st.rerun()
+                    cont = 0
+                    placeholder = st.empty()
+                    while cont <= 300:
+                        if download:
+                            break
+                        placeholder.warning(
+                            f"Tempo de download disponível: {300-cont} segundos"
+                        )
+                        cont += 1
+                        time.sleep(1)
+                # limpando a pasta de downloads do servidor
+                print(
+                    f"[INFO] - {time.strftime('%X')} - Removendo arquivo {arq} [INFO]"
+                )
+                os.remove(f"{PATH_DOWNLOADS}/{arq}")
+                st.subheader("Tempo esgotado, forneca a url novamente")
+                time.sleep(5)
+                st.rerun()
     else:
         st.error("Ocorreu um erro ao baixar o video.")
 footer = """
